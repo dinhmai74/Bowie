4ceaf28a25194ef9b1b700350f2899474507c2f5
dinhmai74 <anh.dinh74@gmail.com>
2020-05-25 23:32:53 +0700

    feed backdrop issue

--- mobile/app/components/Backdrop/Backdrop.tsx
+++ mobile/app/components/Backdrop/Backdrop.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
-import Animated from 'react-native-reanimated'
+import Animated, { block, call, cond, eq, useCode } from 'react-native-reanimated'
 
 const styles = StyleSheet.create({
   // Shadow
@@ -22,9 +22,21 @@ export const Backdrop: React.FunctionComponent<BackdropProps> = props => {
     outputRange: [0.8, 0],
   })
 
+  const [show, setShow] = React.useState(false)
+
+  useCode(() => {
+    return block([
+      cond(
+        eq(fall, 1),
+        call([], () => setShow(false)),
+        call([], () => setShow(true)),
+      ),
+    ])
+  }, [fall])
+
   return (
     <Animated.View
-      pointerEvents="none"
+      pointerEvents={show ? 'auto' : 'none'}
       style={[
         styles.shadowContainer,
         {
